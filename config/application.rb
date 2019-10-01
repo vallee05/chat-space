2ce92cc93a4cd14a99f736b52ba401b5eefbcfee
vallee05 <voluptuous.666@gmail.com>
2019-10-01 20:58:40 +0900

    group-edit

--- config/application.rb
+++ config/application.rb
@@ -8,12 +8,13 @@ Bundler.require(*Rails.groups)
 
 module ChatSpace
   class Application < Rails::Application
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :ja
     config.generators do |g|
       g.stylesheets false
       g.javascripts false
       g.helper false
       g.test_framework false
     end
-    config.i18n.default_locale = :ja
   end
 end
