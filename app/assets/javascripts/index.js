3920c62342c0c86016cf51a4ab6dc294f796a6bd
vallee05 <voluptuous.666@gmail.com>
2019-10-13 18:42:31 +0900

    テキストフィールドに入力されるたびにイベント発火

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,4 +1,12 @@
 $(function() {
+function appendUser(user) {
+  var html = `<div class="chat-group-user clearfix">
+                <p class="chat-group-user__name">${user.name}</p>
+                <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="ユーザーのid" data-user-name="ユーザー名">追加</div>
+              </div>`
+  return html;
+  }
+
   $("#user-search-field").on("keyup", function() {
     var input = $("#user-search-field").val();
 
@@ -7,6 +15,21 @@ $(function() {
       url: '/users/index',
       data: { keyword: input },
       dataType: 'json'
+    })
+
+    .done(function(users){
+      $("#user-search-field").empty();
+      if (users.length !== 0) {
+        users.forEach(function(user){
+          appendUser(user);
+        });
+      }
+      else {
+        appendErrMsgToHTML("一致するユーザーが見つかりません");
+      }
+    })
+    .fail(function(){
+      alert('ユーザー検索に失敗しました');
     })
   });
 });
