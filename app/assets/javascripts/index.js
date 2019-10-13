6b7893fcdba6c7b802c08833726d634ecf436e1d
vallee05 <voluptuous.666@gmail.com>
2019-10-13 17:39:00 +0900

    ルーティング設定　コントローラー設定　jsファイル作成　jbuilderファイル作成

---
+++ app/assets/javascripts/index.js
@@ -0,0 +1,13 @@
+$(function() {
+  $("#user-search-field").on("keyup", function() {
+    var input = $("#user-search-field").val();
+
+    $.ajax({
+      type: 'GET',
+      url: '/users/index',
+      data: { keyword: input },
+      dataType: 'json'
+    })
+  });
+});
+
